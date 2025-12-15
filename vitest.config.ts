740896a1464aa03cf3586326285298d4eff255e8
Brad P <brad@ad-astra.video>
2025-12-15 15:46:19 -0600

    update src files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: [],
+    setupFiles: ['./src/__tests__/mockServer.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
